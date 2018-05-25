28f368dd4022a4c22133804186cc7a332837e1fe
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2018-05-25 14:49:13 +0900

    01-1-2

--- cpp_problem/01-1.cpp
+++ cpp_problem/01-1.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int var1, var2, var3, var4, var5;
-	cout << "1번째 정수 입력: ";
-	cin >> var1;
-	cout << "2번째 정수 입력: ";
-	cin >> var2;
-	cout << "3번째 정수 입력: ";
-	cin >> var3;
-	cout << "4번째 정수 입력: ";
-	cin >> var4;
-	cout << "5번째 정수 입력: ";
-	cin >> var5;
-	cout << "합계: " << var1 + var2 + var3 + var4 + var5 << endl;
+	char name[100];
+	char phone_number[100];
+	cin >> name >> phone_number;
+	cout << name << endl;
+	cout << phone_number << endl;
 	return 0;
 }
