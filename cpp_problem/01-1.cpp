1426d99a94ebb15f62a7f6cec00f6ad92b49a39e
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2018-05-25 14:57:38 +0900

    01-1-4

--- cpp_problem/01-1.cpp
+++ cpp_problem/01-1.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int num;
-	cin >> num;
-	for (int i = 1; i <= 9; i++) {
-		cout << num << " * " << i << " = " << num * i << endl;
+	int sell_price;
+	while (true) {
+		cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
+		cin >> sell_price;
+		if (sell_price == -1) {
+			cout << "프로그램을 종료합니다" << endl;
+			break;
+		}
+		cout << "이번 달 급여: " << 50 + sell_price * 0.12 << "만원" << endl;
 	}
 	return 0;
 }
