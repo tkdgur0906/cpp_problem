6b478472bcec47e8f4deaf6fdaae613d2a5762fe
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2018-05-29 22:29:28 +0900

    01-3

--- cpp_problem/01-1.cpp
+++ cpp_problem/01-1.cpp
@@ -1,34 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *num1, int *num2) {
-	int temp = *num1;
-	*num1 = *num2;
-	*num2 = temp;
+int BoxVolume(int length, int width, int height) {
+	return length * width*height;
 }
-void swap(char *ch1, char *ch2) {
-	char temp = *ch1;
-	*ch1 = *ch2;
-	*ch2 = temp;
+
+int BoxVolume(int length, int width) {
+	return length * width;
 }
-void swap(double *db1, double *db2) {
-	double temp = *db1;
-	*db1 = *db2;
-	*db2 = temp;
+
+int BoxVolume(int length) {
+	return length;
+}
+
+int BoxVolume() {
+	return 1;
 }
 int main() {
-	int num1 = 20, num2 = 30;
-	swap(&num1, &num2);
-	cout << num1 << ' ' << num2 << endl;
+	cout << "[3, 3, 3] :" << BoxVolume(3, 3, 3) << endl;
+	cout << "[5, 5, D] :" << BoxVolume(5,5) << endl;
+	cout << "[7, D, D] :" << BoxVolume(7) << endl;
+	cout << "[D, D, D] :" << BoxVolume() << endl;
 
-	char ch1 = 'A', ch2 = 'Z';
-	swap(&ch1, &ch2);
-	cout << ch1 << ' ' << ch2 << endl;
-
-	double db1 = 1.111, db2 = 5.555;
-	swap(&db1, &db2);
-	cout << db1 << ' ' << db2 << endl;
-}
+	}
 
 
 
