2bd51b07f4e14c39e7a7447dbdd6229bb2cdf249
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2018-06-03 21:06:51 +0900

    02-1-1

--- cpp_problem/01-1.cpp
+++ cpp_problem/01-1.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int BoxVolume(int length, int width, int height) {
-	return length * width*height;
+void plusOne(int &ref) {
+	ref = ref + 1;
 }
 
-int BoxVolume(int length, int width) {
-	return length * width;
+void changeSign(int &ref) {
+	ref = -ref;
 }
 
-int BoxVolume(int length) {
-	return length;
-}
-
-int BoxVolume() {
-	return 1;
-}
 int main() {
-	cout << "[3, 3, 3] :" << BoxVolume(3, 3, 3) << endl;
-	cout << "[5, 5, D] :" << BoxVolume(5,5) << endl;
-	cout << "[7, D, D] :" << BoxVolume(7) << endl;
-	cout << "[D, D, D] :" << BoxVolume() << endl;
-
+	int temp = 10;
+	plusOne(temp);
+	cout << temp << endl;
+	changeSign(temp);
+	cout << temp << endl;
 	}
 
 
